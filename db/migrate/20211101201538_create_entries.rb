e189ef611230367b002aa3d9b85613f47772c8a1
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-01 16:40:32 -0400

    added association

--- db/migrate/20211101201538_create_entries.rb
+++ db/migrate/20211101201538_create_entries.rb
@@ -4,6 +4,7 @@ class CreateEntries < ActiveRecord::Migration[6.1]
       t.string :date
       t.string :mood
       t.string :note 
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
