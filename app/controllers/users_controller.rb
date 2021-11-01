078375b4fc3b7102436986430b93a5d02d267a92
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-01 17:58:20 -0400

    made it possible to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authorized, only: :create
 
   # GET /users
   # def index
@@ -15,10 +16,13 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
+    binding.pry
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode_token(user_id: @user.id)
+      # render json: @user, status: :created, 
+      render json: {user: @user, jwt: @token}
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +50,8 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :username, :email, :password)
+      # params.fetch(:user, {}).permit(:name, :username, :email, :password)
+      # params.require(:user).permit(:name, :username, :email, :password)
+      params.permit(:name, :username, :email, :password)
     end
 end
