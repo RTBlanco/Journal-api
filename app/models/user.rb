0f57c37a2d2203f6342f526f2362bde3c358d8b9
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-10 09:22:28 -0500

    added validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  has_many :entries 
-  
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true 
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+
+  has_many :entries
   has_secure_password
 
   def serialize
     self.attributes.except("password_digest", "created_at", "updated_at")
   end
-
 end
+
