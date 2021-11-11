17e7dbef99dc373b791ddf51dc88acadbf06e738
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-11 17:10:42 -0500

    created validation to save date

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,29 +1,18 @@
-class Entry < ApplicationRecord
-  attr_reader :date
+require "time"
 
+class Entry < ApplicationRecord
+  #TODO: want to save it in the database as a date time instead! 
   belongs_to :user
   
   validates :mood, presence: true
   validates :note, presence: true
   validates :date, presence: true, format: {
     with: /\d{4}\-\d{2}\-\d{2}/,
-    strict: true,
     message: "Date is incorrect format yyyy-mm-dd"
   }
-  
-
 
   def serialize
     self.attributes.except("created_at", "updated_at")
-  end
-
-  # create method that saves the date parses and saves the date 
-  def date
-    #date will come in as 2021-10-10
-    #needs to return a date object
-
-    self.date.to_time
-  
   end
 
 end
