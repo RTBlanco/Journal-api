f58ac50675d7c53fb525311022811f3713254e8f
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-11 16:13:15 -0500

    converted string date to object

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,15 @@
 class Entry < ApplicationRecord
+  attr_reader :date
+
   belongs_to :user
   
   validates :mood, presence: true
   validates :note, presence: true
+  validates :date, presence: true, format: {
+    with: /\d{4}\-\d{2}\-\d{2}/,
+    strict: true,
+    message: "Date is incorrect format yyyy-mm-dd"
+  }
   
 
 
@@ -11,4 +18,12 @@ class Entry < ApplicationRecord
   end
 
   # create method that saves the date parses and saves the date 
+  def date
+    #date will come in as 2021-10-10
+    #needs to return a date object
+
+    self.date.to_time
+  
+  end
+
 end
